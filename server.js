bb45d6c5793582b73db8f7ec7f6edd99d3cbab57
Adrian Mendoza <adsmen13@gmail.com>
2018-07-28 17:49:55 +0800

    Fixed errors (image and css)

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 
 
-const express = require("express")
-const hbs = require("hbs")
-const bodyparser = require("body-parser")
-const session = require("express-session") //for cookies also
-const path = require("path")
-const cookieparser = require("cookie-parser") //for cookies also
+const express = require("express");
+const hbs = require("hbs");
+const bodyparser = require("body-parser");
+const session = require("express-session"); //for cookies also
+const path = require("path");
+const cookieparser = require("cookie-parser"); //for cookies also
 // const mongoose = require("mongoose")
 // const mongostore = require("connect-mongo")(session)
 
@@ -15,23 +15,29 @@ const urlencoder = bodyparser.urlencoded({
     extended: false
 })
 
-app.set("view engine", "hbs")
-app.use(express.static(__dirname+"/views"))
+app.set("view engine", "hbs");
+app.use(express.static(__dirname+"/views"));
+app.use(express.static(__dirname+"/storage/images"));
+app.use(express.static(__dirname+"/css"));
 
-app.use(express.static(path.join(__dirname, "views")))
+//
+// var staticResourse = '/Users/adrianmendoza/Desktop/Git/WEBAPDE-MP/storage/images';
+// app.use(express.static(path.join(staticResourse, 'public2')));
+
+// app.use(express.static(path.join(__dirname, "views")))
 
 app.get("/", (req, res, next) => {
-    console.log("GET /")
+    console.log("GET /");
 
-        res.render("landing.hbs")
+        res.render("landing.hbs");
 //        res.sendFile(path.join(__dirname, "views/landing.hbs"))
 
-    console.log("HI")
-})
+    console.log("HI");
+});
 
 
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log("listening in port 3000")
-})
+});
 
