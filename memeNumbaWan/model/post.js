d035762c0846fc84566c228a2bdb7715456c7db2
Anjo Santos <anjosantos92@gmail.com>
2018-08-27 22:05:30 +0800

    Update model

--- memeNumbaWan/model/post.js
+++ memeNumbaWan/model/post.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId
+//const Schema = mongoose.Schema;
+//const ObjectId = Schema.ObjectId
 
-const PostSchema = new Schema({
 
+const PostSchema = mongoose.Schema({
     title: String,
-    uid: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     user: String,
-    tags: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
-    likers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    unlikers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    tags: [String],
+    likers: [String],
+    unlikers: [String],
     time: {type: Date, default: Date.now},
     privacy: Boolean,
-    sharedTo: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
-    
+    sharedTo: [String]
 })
 
+delete mongoose.connection.models['post'];
+const Post = mongoose.model("post", PostSchema);
 
-const Post = mongoose.model ("post", PostSchema);
 
 module.exports = {
     Post
@@ -25,4 +24,201 @@ module.exports = {
 
 var a = {
     Post
+}
+
+//CREATE NEW POST
+exports.createNew = function(post){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/createNew-----")
+
+    var p = new Post(post)
+
+    p.save().then((newPost)=>{
+      console.log ("*NEW POST CREATED!*")
+
+      resolve(newPost)
+    }, (err)=>{
+      reject(err)
+    })
+  })
+}
+
+//FIND ONE AND UPDATE BY ID ONLY
+exports.updateOneByID = function(id, newPost){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/updateOneByID-----")
+
+    Post.findOneAndUpdate({
+        _id : id
+    }, newPost).then(()=>{
+       console.log ("*POST UPDATED!*")
+//        res.redirect("/redirectprofile")
+    })
+
+// SAMPLE:
+//    Post.findOneAndUpdate({
+//      _id : id
+//    }, update, {
+//      new : true
+//    }).then((newPost)=>{
+//      resolve(newPost)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//FIND ONE AND UPDATE BY ID AND USER (private/public)
+exports.updateOneByUser = function(id, user, newPost){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/updateOneByUser-----")
+
+    Post.findOneAndUpdate({
+        _id : id,
+        user : user
+    }, newPost).then(()=>{
+       console.log ("*POST UPDATED!*")
+//        res.redirect("/redirectprofile")
+    })
+
+// SAMPLE:
+//    Post.findOneAndUpdate({
+//      _id : id
+//    }, update, {
+//      new : true
+//    }).then((newPost)=>{
+//      resolve(newPost)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//FIND ONE AND PUSH TAGS
+exports.updateAndPush = function(id, tag){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/updateAndPush-----")
+
+    Post.findOneAndUpdate({
+        _id : id
+    },{
+        $push: {tags : tag}
+    },{
+        new: true
+    }, function(err, doc){
+        if(err){
+            console.log ("*POST NOT UPDATED!*")
+        }
+        console.log(doc);
+    });
+  })
+}
+
+//GET ALL POSTS
+exports.getAll = function(){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/getAll-----")
+
+    Post.find().then((posts)=>{
+       console.log ("*GOT ALL POSTS!*")
+       resolve(posts)
+//      res.render("home.hbs", {
+//        posts : posts,
+//      });
+    }, (err)=>{
+       console.log ("*DID NOT GET ALL POSTS!*")
+       reject(err)
+
+//      res.render("landing.hbs")
+    })
+
+// SAMPLE:
+//    Post.find().then((posts)=>{
+//      resolve(posts)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//GET ALL POSTS FILTERED BY NAME
+exports.getAllFiltered = function(uname){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/getAll-----")
+
+    Post.find({
+        user : req.session.username
+    }).then((posts)=>{
+       console.log ("*GOT ALL POSTS!*")
+       resolve(posts)
+//      res.render("home.hbs", {
+//        posts : posts,
+//      });
+    }, (err)=>{
+       console.log ("*DID NOT GET ALL POSTS!*")
+       reject(err)
+
+//      res.render("landing.hbs")
+    })
+
+// SAMPLE:
+//    Post.find().then((posts)=>{
+//      resolve(posts)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//GET ONE POST
+exports.getOne = function(id){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/getOne-----")
+
+    Post.findOne({
+      _id: id
+    }).then((post)=>{
+       console.log ("*GOT ONE POST!*")
+
+//      res.render("meme.hbs", {
+//          post
+//      })
+    }, (err)=>{
+       console.log ("*POST DOES NOT EXIST!*")
+
+        reject(err)
+    })
+
+// SAMPLE:
+//    Post.findOne({_id:id}).then((post)=>{
+//      console.log(post)
+//      resolve(post)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//DELETE POST
+exports.deletePost = function(id){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/post/deletePost-----")
+
+    Post.deleteOne({
+      _id: id
+    }).then((post)=>{
+       console.log ("*POST DELETED!*")
+
+//      res.redirect("/redirectprofile")
+    })
+
+// SAMPLE:
+//    Post.remove({
+//      _id : id
+//    }).then((result)=>{
+//      resolve(result)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
 }
--- memeNumbaWan/model/user.js
+++ memeNumbaWan/model/user.js
@@ -3,24 +3,28 @@ const Schema = mongoose.Schema;
 
 //Create user schema and model
 
+//const PostSchema = new Schema({
+//    title: String,
+//    tags: [String],
+//    likers: [String],
+//    unlikers: [String],
+//    time: {type: Date, default: Date.now},
+//    privacy: Boolean,
+//    sharedTo: [String]
+//})
+
 const UserSchema = new Schema({
-    username: String,
-    name: String,
+    username: {
+        type:String,
+        index: true
+    },
     password: String,
     description: String,
-    postID : [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
-    
-//    img: {
-//        type: String,
-//        default: '/img/samples/sample_profile.jpg',
-//    },
-//    postIDs : {
-//        type : Array,
-//        default: null
-//    }
+    posts : [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
     
 })
 
+delete mongoose.connection.models['user'];
 const User = mongoose.model ("user", UserSchema);
 
 module.exports = {
@@ -30,3 +34,124 @@ module.exports = {
 var a = {
     User
 }
+
+//CREATE NEW USER
+exports.createNew = function(user){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/user/createNew-----")
+
+    var u = new User(user)
+
+    u.save().then(()=>{
+      console.log ("*NEW USER CREATED!*")
+//        res.redirect("/redirectprofile")
+    }, (err)=>{
+      console.log ("*NEW USER NOT CREATED!*")
+//        res.render("landing.hbs")
+    })
+
+//    p.save().then((newPost)=>{
+//      resolve(newPost)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//GET ONE USER BY USERNAME & PASSWORD
+module.exports.getOneByUnameAndPword = function(uname, pword){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/user/getOneByUnameAndPword-----")
+
+    User.findOne({
+            username : uname,
+            password : pword
+    }).then((user)=>{
+        if(user != null){
+           console.log ("*GOT ONE USER!*")
+
+//            console.log(user.username + ": " + user.password);
+//            req.session.username = user.username;
+//            console.log("uname sesh: "+req.session.username);
+//            res.redirect("/")
+        }
+        else{
+           console.log ("*USER DOES NOT EXIST!*")
+
+//            res.redirect("/")
+//            console.log("cannot login")
+        }
+    }, (err)=>{
+        console.log ("*USER DOES NOT EXIST!*")
+//        res.render("landing.hbs")
+    })
+
+// SAMPLE:
+//    Post.findOne({_id:id}).then((post)=>{
+//      console.log(post)
+//      resolve(post)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+
+//GET ONE USER BY USERNAME
+module.exports.getOneByUname = function(uname){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/user/getOneByUnameAndPword-----")
+
+    User.findOne({
+            username : uname
+    }).then((user)=>{
+        if(user != null){
+           console.log ("*GOT ONE USER!*")
+
+//            console.log(user.username + ": " + user.password);
+//            req.session.username = user.username;
+//            console.log("uname sesh: "+req.session.username);
+//            res.redirect("/")
+        }
+        else{
+           console.log ("*USER DOES NOT EXIST!*")
+
+//            res.redirect("/")
+//            console.log("cannot login")
+        }
+    }, (err)=>{
+        console.log ("*USER DOES NOT EXIST!*")
+//        res.render("landing.hbs")
+    })
+
+// SAMPLE:
+//    Post.findOne({_id:id}).then((post)=>{
+//      console.log(post)
+//      resolve(post)
+//    }, (err)=>{
+//      reject(err)
+//    })
+  })
+}
+
+//FIND ONE AND PUSH POSTS
+exports.updateAndPush = function(id, post){
+  return new Promise(function(resolve, reject){
+    console.log ("-----model/user/updateAndPush-----")
+
+    User.findOneAndUpdate({
+        _id : id
+    },{
+        $push: {posts : post}
+    },{
+        new: true
+    }, function(err, doc){
+        if(err){
+            console.log ("*USER NOT UPDATED!*")
+        }
+        console.log(doc);
+    });
+  })
+}
+
+
